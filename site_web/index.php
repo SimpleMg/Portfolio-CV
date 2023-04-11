1ea71fcb56618852d3c682db36e31dccaa1c2f15
Mortimer <mortimer@mortimer.com>
2023-04-11 12:19:33 +0200

    ajout video/modif navbar couleur

--- site_web/index.php
+++ site_web/index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CDN BOOSTRAP CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
     <title>Portfolio</title>
 </head>
 <body>
@@ -14,7 +15,7 @@
       $dossier = __DIR__;
     ?>
     <!-- NAVBAR -->
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">Portfolio</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,7 +24,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">acceuil</a>
+              <a class="nav-link active" aria-current="page" href="#acceuil_ref">acceuil</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#cv_ref">cv</a>
@@ -43,9 +44,15 @@
     </nav>
     <!-- FIN NAVBAR -->
     <!-- Acceuil -->
-    <p id="acceuil_ref">acceuil</p>
+    <!-- Video -->
+    <section id="acceuil_ref">
+        <div class="video-wrapper">
+            <video width="100%" playsinline autoplay muted loop>
+            <source src="assets/video/Blue Black Futuristic Technology Video.mp4">Votre navigateur ne supporte pas le player vidéo</video>
+        </div>
+    </section>
     <!-- CV -->
-    <p id="cv_ref">CV</p>
+    <section class="container-fluid" id="cv_ref">
     <?php 
         $fichier = '/cv.php';
         if (file_exists($dossier . $fichier)) {
@@ -54,7 +61,9 @@
             echo "Le fichier $fichier n'a pas été trouvé pas";
         }
     ?>
+    </section>
     <!-- projets -->
+    <section class="container-fluid" id="projets_ref">
     <p id="projets_ref">projets</p>
     <?php 
         $fichier = '/projets.php';
@@ -64,7 +73,9 @@
             echo "Le fichier $fichier n'a pas été trouvé pas";
         }
     ?>
+    </section>
     <!-- jeux -->
+    <section class="container-fluid" id="jeux_ref">
     <p id="jeux_ref">jeux</p>
     <?php 
         $fichier = '/jeux.php';
@@ -74,7 +85,9 @@
             echo "Le fichier $fichier n'a pas été trouvé pas";
         }
     ?>
+    </section>
     <!-- contact -->
+    <section class="container-fluid" id="contact_ref">
     <p id="contact_ref">contact</p>
     <?php 
         $fichier = '/contact.php';
@@ -84,6 +97,7 @@
             echo "Le fichier $fichier n'a pas été trouvé pas";
         }
     ?>
+    </section>
     <!-- CDN BOOSTRAP JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
