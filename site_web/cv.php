3f333fb2bbff1a4cff1933ff26a251efaa4dd85c
Mortimer <mortimer@mortimer.com>
2023-04-11 12:56:22 +0200

    ajout cv

--- site_web/cv.php
+++ site_web/cv.php
@@ -1,3 +1,72 @@
-<?php 
-echo "cv.php";
-?>
+
+  <div class="container">
+    <h1 class="text-center">Mon Curriculum Vitae</h1>
+    <hr>
+
+    <!-- Informations personnelles -->
+    <h2 class="section-title">Informations personnelles</h2>
+    <div class="row">
+      <div class="col-md-6">
+        <p><strong>Nom :</strong> Sam Levy</p>
+        <p><strong>Email :</strong> samlevypro@proton.me</p>
+      </div>
+    </div>
+    <hr>
+
+    <!-- Compétences -->
+    <h2 class="section-title">Compétences</h2>
+    <ul class="list-group">
+      <li class="list-group-item">HTML/Boostrap</li>
+      <li class="list-group-item">PHP</li>
+      <li class="list-group-item">Python</li>
+      <li class="list-group-item">C</li>
+    </ul>
+    <hr>
+<!-- Expreience pro
+    <h2 class="section-title">Expérience professionnelle</h2>
+    <div class="row">
+      <div class="col-md-6">
+        <h3>Terrasses du trocadero</h3>
+        <p><strong>Poste :</strong> Manutentionnaire</p>
+        <p><strong>Date :</strong> Mois Année - Mois Année</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris.</p>
+      </div>
+      <div class="col-md-6">
+        <h3>Entreprise B</h3>
+        <p><strong>Poste :</strong> Titre du poste</p>
+        <p><strong>Date :</strong> Mois Année - Mois Année<    :</strong> Mois Année - Mois Année</p>
+    <p>Curabitur feugiat ullamcorper sapien, ut sodales sem tempor vitae. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.</p>
+  </div>
+</div>
+<hr>
+-->
+<!-- Formation -->
+<h2 class="section-title">Formation</h2>
+<div class="row">
+  <div class="col-md-6">
+    <h3>UVSQ</h3>
+    <p><strong>Diplôme :</strong> Licence Mathématiques informatiques</p>
+    <p><strong>Date :</strong> 11/07/2023</p>
+  </div>
+  <div class="col-md-6">
+    <h3>Guardia Cybersecurity</h3>
+    <p><strong>Diplôme :</strong> Bachelor développeur informatique option cybersécurité</p>
+    <p><strong>Date :</strong> 11/07/2023</p>
+  </div>
+</div>
+<hr>
+
+<!-- Langues -->
+<h2 class="section-title">Langues</h2>
+<ul class="list-group">
+  <li class="list-group-item">Français : langue maternelle</li>
+  <li class="list-group-item">Anglais : intermédiaire</li>
+  <li class="list-group-item">Russe : Notions</li>
+</ul>
+<hr>
+<!-- Centres d'intérêt -->
+<h2 class="section-title">Centres d'intérêt</h2>
+<p>Cybersécurité</p>
+<p>Programmation</p>
+<p>Sport</p>
+</div>
