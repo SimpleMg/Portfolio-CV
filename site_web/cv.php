e108b40ba90c08afa66aac66f430ba9c7b4110cd
SimpleMg <117079680+SimpleMg@users.noreply.github.com>
2023-04-12 22:47:47 +0200

    Update cv.php

--- site_web/cv.php
+++ site_web/cv.php
@@ -1,3 +1,49 @@
+<style>
+  body {
+    background-color: #0d0d0d;
+    color: #32cd32;
+    font-family: "Courier New", monospace;
+  }
+
+  h1,
+  h2,
+  h3 {
+    font-family: 'Roboto', sans-serif;
+    color: #32cd32;
+  }
+
+  .marker {
+    position: relative;
+    display: inline;
+  }
+
+  .marker-center::before {
+    content: '';
+    position: absolute;
+    background-color: #32cd32;
+    height: 2px;
+    width: 100%;
+    bottom: 0;
+    left: 0;
+  }
+
+  img {
+    border-radius: 50%;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  }
+
+  p {
+    font-size: 1.1rem;
+    line-height: 1.6;
+    color: #50e050;
+  }
+
+  .skill-icon {
+    font-size: 10rem;
+    margin-bottom: 10px;
+    color: #32cd32;
+  }
+</style>
 <div class="section pt-4 px-3 px-lg-4" id="about">
   <div class="container-narrow mx-auto">
     <div class="row">
@@ -6,22 +52,22 @@
           <br>
           <br>
           <p>
-            Je suis Sam et j'ai développé un grand intérêt pour la cyber sécurité et la programmation.
-            Mon envie de comprendre le fonctionnement des ordinateurs m'a conduit à explorer les différents 
-            aspects de ces domaines fascinants. J'adore la complexité des problèmes à résoudre en matière de sécurité informatique, 
+            Je m'appelle Sam et j'ai développé un grand intérêt pour la cyber sécurité et la programmation.
+            Mon envie de comprendre le fonctionnement des ordinateurs m'a conduit à explorer les différents
+            aspects de ces domaines fascinants. J'adore la complexité des problèmes à résoudre en matière de sécurité informatique,
             ainsi que les possibilités infinies que la programmation offre pour créer des systèmes innovants.
             Mon engagement envers ces domaines ne cesse de grandir au fil des années, et j'ai décidé de me consacrer pleinement à leur exploration.
             Je suis constamment en train d'apprendre de nouvelles choses pour rester à jour avec les dernières tendances et techniques de programmation
-            et de sécurité. J'ai également une passion pour l'échange et le partage des connaissances. 
-            J'aime discuter avec d'autres professionnels et passionnés pour élargir mes horizons et partager mes découvertes. 
-            Je suis convaincu que l'ouverture d'esprit et la collaboration sont les clés de la réussite dans ces domaines, 
+            et de sécurité. J'ai également une passion pour l'échange et le partage des connaissances.
+            J'aime discuter avec d'autres professionnels et passionnés pour élargir mes horizons et partager mes découvertes.
+            Je suis convaincu que l'ouverture d'esprit et la collaboration sont les clés de la réussite dans ces domaines,
             et je suis heureux de faire partie d'une communauté aussi dynamique et innovante.
           </p>
         </div>
       </div>
       <div class="col-md-5 offset-md-1" data-aos="fade-left" data-aos-delay="100">
         <div class="px-3">
-          <img class="avatar img-fluid mt-2" src="assets/img/sam_l.jpg" width="400" height="400" alt="Sam Levy"/>
+          <img class="avatar img-fluid mt-2" src="assets/img/sam_l.jpg" width="400" height="400" alt="Sam Levy" />
         </div>
       </div>
     </div>
@@ -35,37 +81,40 @@
     <div class="row">
       <div class="col-md-4">
         <div class="skill-block text-center">
+          <i class="fab fa-html5 skill-icon"></i>
           <h3>HTML</h3>
         </div>
       </div>
       <div class="col-md-4">
         <div class="skill-block text-center">
+          <i class="fab fa-bootstrap skill-icon"></i>
           <h3>Bootstrap</h3>
         </div>
       </div>
       <div class="col-md-4">
         <div class="skill-block text-center">
+          <i class="fab fa-python skill-icon"></i>
           <h3>Python</h3>
         </div>
       </div>
-    </div>
-    <div class="row">
       <div class="col-md-4">
         <div class="skill-block text-center">
+          <i class="fab fa-php skill-icon"></i>
           <h3>PHP</h3>
         </div>
       </div>
       <div class="col-md-4">
         <div class="skill-block text-center">
-          <h3>C</h3>
+          <i class="fab fa-cuttlefish skill-icon"></i>
+          <h3>Langage C</h3>
         </div>
       </div>
       <div class="col-md-4">
         <div class="skill-block text-center">
+          <i class="fas fa-database skill-icon"></i>
           <h3>SQL</h3>
         </div>
       </div>
     </div>
   </div>
 </div>
-
