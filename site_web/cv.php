ba31cd641401fde836c734401060724f04283c95
Mortimer <mortimer@mortimer.com>
2023-04-12 17:16:27 +0200

    modif cv

--- site_web/cv.php
+++ site_web/cv.php
@@ -1,70 +1,78 @@
-<div class="container">
-    <h1 class="text-center">Mon Curriculum Vitae</h1>
-    <hr>
-
-    <!-- Informations personnelles -->
-    <h2 class="section-title">Informations personnelles</h2>
+<div class="section pt-4 px-3 px-lg-4" id="about">
+  <div class="container-narrow mx-auto">
     <div class="row">
       <div class="col-md-6">
-        <p><strong>Nom :</strong> Sam Levy</p>
-        <p><strong>Email :</strong> samlevypro@proton.me</p>
+        <div class="px-3">
+          <br>
+          <br>
+          <p>
+            Je suis Sam et j'ai développé un grand intérêt pour la cyber sécurité et la programmation.
+            Mon envie de comprendre le fonctionnement des ordinateurs m'a conduit à explorer les différents 
+            aspects de ces domaines fascinants. J'adore la complexité des problèmes à résoudre en matière de sécurité informatique, 
+            ainsi que les possibilités infinies que la programmation offre pour créer des systèmes innovants.
+            Mon engagement envers ces domaines ne cesse de grandir au fil des années, et j'ai décidé de me consacrer pleinement à leur exploration.
+            Je suis constamment en train d'apprendre de nouvelles choses pour rester à jour avec les dernières tendances et techniques de programmation
+            et de sécurité. J'ai également une passion pour l'échange et le partage des connaissances. 
+            J'aime discuter avec d'autres professionnels et passionnés pour élargir mes horizons et partager mes découvertes. 
+            Je suis convaincu que l'ouverture d'esprit et la collaboration sont les clés de la réussite dans ces domaines, 
+            et je suis heureux de faire partie d'une communauté aussi dynamique et innovante.
+          </p>
+        </div>
+      </div>
+      <div class="col-md-5 offset-md-1" data-aos="fade-left" data-aos-delay="100">
+        <div class="px-3">
+          <img class="avatar img-fluid mt-2" src="assets/img/sam_l.jpg" width="400" height="400" alt="Sam Levy"/>
+        </div>
       </div>
     </div>
-    <hr>
-
-    <!-- Compétences -->
-    <h2 class="section-title">Compétences</h2>
-    <ul class="list-group">
-      <li class="list-group-item">HTML/Boostrap</li>
-      <li class="list-group-item">PHP</li>
-      <li class="list-group-item">Python</li>
-      <li class="list-group-item">C</li>
-    </ul>
-    <hr>
-    <!-- Expreience pro
-        <h2 class="section-title">Expérience professionnelle</h2>
-        <div class="row">
-        <div class="col-md-6">
-            <h3>Terrasses du trocadero</h3>
-            <p><strong>Poste :</strong> Manutentionnaire</p>
-            <p><strong>Date :</strong> Mois Année - Mois Année</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris.</p>
-        </div>
-        <div class="col-md-6">
-            <h3>Entreprise B</h3>
-            <p><strong>Poste :</strong> Titre du poste</p>
-            <p><strong>Date :</strong> Mois Année - Mois Année<    :</strong> Mois Année - Mois Année</p>
-        <p>Curabitur feugiat ullamcorper sapien, ut sodales sem tempor vitae. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.</p>
-    </div>
-    </div>
-    <hr>
-    -->
-    <!-- Formation -->
-    <h2 class="section-title">Formation</h2>
-    <div class="row">
-    <div class="col-md-6">
-        <h3>UVSQ</h3>
-        <p><strong>Diplôme :</strong> Licence Mathématiques informatiques</p>
-        <p><strong>Date :</strong> 11/07/2023</p>
-    </div>
-    <div class="col-md-6">
-        <h3>Guardia Cybersecurity</h3>
-        <p><strong>Diplôme :</strong> Bachelor développeur informatique option cybersécurité</p>
-        <p><strong>Date :</strong> 11/07/2023</p>
-    </div>
-    </div>
-    <hr>
-    <!-- Langues -->
-    <h2 class="section-title">Langues</h2>
-    <ul class="list-group">
-    <li class="list-group-item">Français : langue maternelle</li>
-    <li class="list-group-item">Anglais : intermédiaire</li>
-    <li class="list-group-item">Russe : Notions</li>
-    </ul>
-    <hr>
-    <!-- Centres d'intérêt -->
-    <h2 class="section-title">Centres d'intérêt</h2>
-    <p>Cybersécurité</p>
-    <p>Programmation</p>
-    <p>Sport</p>
+  </div>
 </div>
+<div class="section px-3 px-lg-4 pt-5">
+  <div class="container-narrow">
+    <div class="text-center mb-5">
+      <h2 class="marker marker-center">Mes compétences</h2>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="skill-block text-center">
+          <h3>HTML</h3>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="skill-block text-center">
+          <h3>Bootstrap</h3>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="skill-block text-center">
+          <h3>Python</h3>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="skill-block text-center">
+          <h3>PHP</h3>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="skill-block text-center">
+          <h3>C</h3>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="skill-block text-center">
+          <h3>SQL</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
--- site_web/projets.php
+++ site_web/projets.php
@@ -1,3 +1,8 @@
+<br>
+<br>
+<br>
+<br>
+<br>
 <div id="carouselExampleCaptions" class="carousel slide">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -36,3 +41,16 @@
     <span class="visually-hidden">Next</span>
   </button>
 </div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
