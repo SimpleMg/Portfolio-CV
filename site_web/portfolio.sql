ac30925599973d8e5c80dedd5c8d27a325eb00be
SimpleMg <117079680+SimpleMg@users.noreply.github.com>
2023-04-17 12:13:03 +0200

    Add files via upload

---
+++ site_web/portfolio.sql
@@ -0,0 +1,112 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : lun. 17 avr. 2023 à 10:12
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `portfolio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(10) NOT NULL,
+  `passwordH` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `passwordH`) VALUES
+(1, 'admin', '$2y$10$oTjVEZHV/VdTBs71GJIFWe9KLu10dLYIGHlRSg.8/vUtY13Ckis5K');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `projets`
+--
+
+DROP TABLE IF EXISTS `projets`;
+CREATE TABLE IF NOT EXISTS `projets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `img` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `projets`
+--
+
+INSERT INTO `projets` (`id`, `titre`, `description`, `img`) VALUES
+(1, 'morpion algo minmax', 'morpion coder en python avec l\'algorithme min max', 'assets/img/morpion.jpg'),
+(2, 'morpion algo minmax', 'morpion coder en python avec l\'algorithme min max', 'assets/img/morpion.jpg'),
+(3, 'morpion algo minmax', 'morpion coder en python avec l\'algorithme min max', 'assets/img/morpion.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `titre`
+--
+
+DROP TABLE IF EXISTS `titre`;
+CREATE TABLE IF NOT EXISTS `titre` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `titre`
+--
+
+INSERT INTO `titre` (`id`, `titre`) VALUES
+(1, 'Portfolio Sam');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `video`
+--
+
+DROP TABLE IF EXISTS `video`;
+CREATE TABLE IF NOT EXISTS `video` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `video` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `video`
+--
+
+INSERT INTO `video` (`id`, `video`) VALUES
+(1, 'assets/video/Blue Black Futuristic Technology Video.mp4');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
