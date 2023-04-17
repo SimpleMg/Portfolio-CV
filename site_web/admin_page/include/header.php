27fbb295056569fdd6a35d88f2313b9a6b5d5d3c
SimpleMg <117079680+SimpleMg@users.noreply.github.com>
2023-04-17 10:34:49 +0200

    Update header.php

--- site_web/admin_page/include/header.php
+++ site_web/admin_page/include/header.php
@@ -1,30 +1,21 @@
-<header>
-    <nav class="navbar navbar-expand-lg navbar-dark hacking-theme">
-    <div class="container">
-        <a class="navbar-brand" href="#">Admin Hacking</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+<nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark fixed-top" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Admin</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-            <a class="nav-link" href="#">Accueil</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#titre_ref">Titre</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#video_ref">Video</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#projets_ref">Projets</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#reseaux_ref">Réseaux</a>
-            </li>
-        </ul>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#acceuil_ref">titre</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#cv_ref">video</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#projets_ref">projets</a>
+                </li>
+            </ul>
         </div>
     </div>
-    </nav>
-</header>
-
+</nav>
