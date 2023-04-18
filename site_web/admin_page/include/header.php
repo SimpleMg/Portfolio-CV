5780d497aa7d1470a9c1ab97ea0a025266a93fca
SimpleMg <117079680+SimpleMg@users.noreply.github.com>
2023-04-18 14:50:40 +0200

    Update header.php

--- site_web/admin_page/include/header.php
+++ site_web/admin_page/include/header.php
@@ -1,3 +1,19 @@
+<?php
+function cleanInput($input)
+{
+    $input = trim($input);
+    $input = stripslashes($input);
+    $input = htmlspecialchars($input);
+    return $input;
+}
+
+$db = new mysqli('localhost', 'root', '', 'portfolio');
+if ($db->connect_errno) {
+    echo "Failed to connect to MySQL: " . $db->connect_error;
+    exit();
+}
+?>
+
 <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark fixed-top" data-bs-theme="dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Admin</a>
@@ -7,13 +23,13 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#acceuil_ref">titre</a>
+                    <a class="nav-link active" aria-current="page" href="#modif_titre">titre</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#cv_ref">video</a>
+                    <a class="nav-link" href="#modif_video">video</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#projets_ref">projets</a>
+                    <a class="nav-link" href="#modif_projets">projets</a>
                 </li>
             </ul>
         </div>
